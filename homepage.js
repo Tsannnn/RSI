29035d67dabd90c4bfe5d0ed608d6d7176522daa
Tsannnn <alfitsani84@gmail.com>
2024-10-26 17:22:28 +0700

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,6 @@
+function openForm3() {
+    document.getElementById("searchForm").style.display = "block";
+}
+function closeForm3() {
+    document.getElementById("searchForm").style.display = "none";
+}
