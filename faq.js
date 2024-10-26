70db2f39f3387e9b6917a1ec4a216f45ed10ba39
Tsannnn <alfitsani84@gmail.com>
2024-10-26 17:22:50 +0700

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,42 @@
+
+// popup add faq
+function openForm1() {
+    document.getElementById("addingForm").style.display = "block";
+}
+
+function closeForm1() {
+    document.getElementById("addingForm").style.display = "none";
+}
+
+// popup delete
+function openForm() {
+    document.getElementById("deleteForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("deleteForm").style.display = "none";
+}
+
+// popup update
+function openForm2() {
+    document.getElementById("editForm").style.display = "block";
+}
+
+function closeForm2() {
+    document.getElementById("editForm").style.display = "none";
+}
+
+// popup search
+function openForm3() {
+    document.getElementById("searchForm").style.display = "block";
+}
+function closeForm3() {
+    document.getElementById("searchForm").style.display = "none";
+}
+
+// konten slide
+function toggleAnswer(question) {
+    question.classList.toggle("active");
+}
+
+
